615e8bbfd3f20fd707c849fdb0db1056f99b7342
Anand-Royy <73389636+Anand-Royy@users.noreply.github.com>
2023-03-20 22:20:15 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(errorHandler);
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
